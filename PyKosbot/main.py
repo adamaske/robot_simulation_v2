e97d4efff001bc9ba9707170314720b067496b5f
Adam Aske <adamaske@live.no>
2024-03-11 05:20:21 +0100

    initlized python side

---
+++ PyKosbot/main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+if __name__ == "__main__":
+    print("Welcome to PyKosbot!\nThe terminal interface for Robot Simulation Version 2.")
+
+    #connect to tcp
+    
+    #not sure if this should be a server or client

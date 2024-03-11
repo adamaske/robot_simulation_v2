2f512f620527df297c1636a47a1e2b4b5f77df5b
Adam Aske <adamaske@live.no>
2024-03-11 23:38:22 +0100

    asd

--- PyKosbot/main.py
+++ PyKosbot/main.py
@@ -27,11 +27,39 @@ def EstablishFakeServer():
                 conn.sendall(data)
             print("FakeServer : Connection dropped...")
 
-   
-        
+
+def Test1():
+    print("Test 1")
+def Test2():
+    print("Test 2")
+def Test3():
+    print("Test 3")
+
+
 if __name__ == "__main__":
     print("Welcome to PyKosbot!\nThe terminal interface for Robot Simulation Version 2.")
-
+    
+    
+    run = True
+    while run:
+        print("What would you like to do?")
+        print(f"1. Test 1")
+        print(f"2. Test 2")
+        print(f"3. Test 3")
+        
+        ans = int(input("Answer (int): ")) 
+        if ans < 1 or ans > 3:
+            run = False
+        if ans == 1:
+            Test1()
+        if ans == 2:
+            Test2()
+        if ans == 3:
+            Test3()
+            
+    print(f"Closing...")
+    exit()
+    
     fake_conn = False
     if fake_conn:
         server = Thread(target=EstablishFakeServer)
@@ -47,12 +75,23 @@ if __name__ == "__main__":
     print("Connection successfull.")
     print("Connected to Kosbot Server on port ", PORT, "...")
     
+    client.close()
     client.sendall(b"Kosbot?")
     data = client.recv(1024)
 
     print(f"Received {data!r}")
     
+    idx = 0
+    opts = [(1, "Hello ", Tetst)]
+    map(lambda x, y, z: print_opt(y, x), opts)
+    
     ans = input("What would you like to do?")
+    ans = int(ans)
+    
+    if ans < len(opts):
+        print("Invalid...")
+    
+    
     if ans == "q":
 
         server.join()
